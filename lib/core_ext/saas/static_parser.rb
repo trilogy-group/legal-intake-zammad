6de3cfd86a49fbabe9040cb0ca947d6f95aa1801
Martin Gruner <mg@zammad.com>
2025-08-18 12:55:14 +0200

    Maintenance: Fix Ruby frozen string warnings

---
+++ lib/core_ext/saas/static_parser.rb
@@ -0,0 +1,42 @@
+# Copyright (C) 2012-2025 Zammad Foundation, https://zammad-foundation.org/
+
+# Monkey patch for frozen literal string warning
+
+require 'sass'
+
+$VERBOSE = false
+
+# rubocop:disable Layout/EmptyLineAfterGuardClause
+# rubocop:disable Zammad/PreferNegatedIfOverUnless
+# rubocop:disable Style/RegexpLiteral
+# rubocop:disable Layout/SpaceInsideBlockBraces
+# rubocop:disable Performance/CollectionLiteralInLoop
+
+module Sass
+  module SCSS
+    class StaticParser < Parser
+      def selector_comma_sequence
+        sel = selector
+        return unless sel
+        selectors = [sel]
+        ws = +''
+        while tok(/,/)
+          ws << str {ss}
+          next unless (sel = selector)
+          selectors << sel
+          if ws.include?("\n")
+            selectors[-1] = Selector::Sequence.new(["\n"] + selectors.last.members)
+          end
+          ws = +''
+        end
+        Selector::CommaSequence.new(selectors)
+      end
+    end
+  end
+end
+
+# rubocop:enable Layout/EmptyLineAfterGuardClause
+# rubocop:enable Zammad/PreferNegatedIfOverUnless
+# rubocop:enable Style/RegexpLiteral
+# rubocop:enable Layout/SpaceInsideBlockBraces
+# rubocop:enable Performance/CollectionLiteralInLoop
