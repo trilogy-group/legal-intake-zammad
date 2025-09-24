b18da133cbf69513921d343039dd9b97e4a95f88
Rolf Schmidt <rolf.schmidt@zammad.com>
2025-09-24 10:25:44 +0200

    Fixes #5785 - Case insensitive in customer mail using the API not always true.
    Co-authored-by: Florian Liebe <fl@zammad.com>

--- app/models/application_model/can_associations.rb
+++ app/models/application_model/can_associations.rb
@@ -93,7 +93,7 @@ returns
             lookup = class_object.lookup(login: value)
           end
           if !lookup
-            lookup = class_object.lookup(email: value)
+            lookup = class_object.lookup(email: value.downcase)
           end
         else
           lookup = class_object.lookup(name: value)
@@ -387,7 +387,7 @@ returns
               lookup = class_object.lookup(login: value)
             end
             if !lookup
-              lookup = class_object.lookup(email: value)
+              lookup = class_object.lookup(email: value.downcase)
             end
           else
             lookup = class_object.lookup(name: value)
@@ -434,7 +434,7 @@ returns
               lookup = class_object.lookup(login: item)
             end
             if !lookup
-              lookup = class_object.lookup(email: item)
+              lookup = class_object.lookup(email: item.downcase)
             end
           else
             lookup = class_object.lookup(name: item)
