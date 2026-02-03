651645d77cd8aafcaa6ca1bfad5faba4e7f37b85
Florian Liebe <fl@zammad.com>
2026-02-03 14:41:26 +0100

    Maintenance: Improve ticket update handling using REST API/GraphQL mutation.
    Co-authored-by: Dominik Klein <dk@zammad.com>

--- app/graphql/gql/types/input/ticket/update_input_type.rb
+++ app/graphql/gql/types/input/ticket/update_input_type.rb
@@ -5,7 +5,13 @@ module Gql::Types::Input::Ticket
     description 'Represents the ticket attributes to be used in ticket update.'
 
     # Arguments optional in update.
-    argument :group_id, GraphQL::Types::ID, required: false, description: 'The group of the ticket.', loads: Gql::Types::GroupType
+    argument :group_id,
+             GraphQL::Types::ID,
+             required:    false,
+             description: 'The group of the ticket.',
+             loads:       Gql::Types::GroupType,
+             prepare:     only_for_ticket_agents
+
     argument :title, Gql::Types::NonEmptyStringType, required: false, description: 'The title of the ticket.'
 
     # Arguments specific to update.
