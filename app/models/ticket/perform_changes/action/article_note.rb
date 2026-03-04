fee752b1a10c2bd16b98c83873bf537147c5bc13
Rolf Schmidt <rolf.schmidt@zammad.com>
2026-03-04 12:55:22 +0100

    Revert "Fixes #5918 - Inline images broken when using article variables."
    Co-authored-by: Florian Liebe <fl@zammad.com>

--- app/models/ticket/perform_changes/action/article_note.rb
+++ app/models/ticket/perform_changes/action/article_note.rb
@@ -7,28 +7,34 @@ class Ticket::PerformChanges::Action::ArticleNote < Ticket::PerformChanges::Acti
   end
 
   def execute(...)
-    add_note
+    add_note(execution_data)
   end
 
   private
 
-  def add_note
+  def add_note(note)
     rendered_subject = NotificationFactory::Mailer.template(
-      templateInline: execution_data[:subject],
+      templateInline: note[:subject],
       objects:        notification_factory_template_objects,
       quote:          true,
       locale:         locale,
       timezone:       timezone,
     )
 
-    (body, attachments_inline) = article_body
+    rendered_body = NotificationFactory::Mailer.template(
+      templateInline: note[:body],
+      objects:        notification_factory_template_objects,
+      quote:          true,
+      locale:         locale,
+      timezone:       timezone,
+    )
 
-    new_article = Ticket::Article.new(
+    article = Ticket::Article.new(
       ticket_id:     id,
       subject:       rendered_subject,
       content_type:  'text/html',
-      body:          body,
-      internal:      execution_data[:internal],
+      body:          rendered_body,
+      internal:      note[:internal],
       sender:        Ticket::Article::Sender.find_by(name: 'System'),
       type:          Ticket::Article::Type.find_by(name: 'note'),
       preferences:   {
@@ -38,32 +44,7 @@ class Ticket::PerformChanges::Action::ArticleNote < Ticket::PerformChanges::Acti
       updated_by_id: 1,
       created_by_id: 1,
     )
-    new_article.history_change_source_attribute(performable, 'created')
-    new_article.save!
-
-    attachments_inline.each do |attachment|
-      Store.create!(
-        object:      'Ticket::Article',
-        o_id:        new_article.id,
-        data:        attachment[:data],
-        filename:    attachment[:filename],
-        preferences: attachment[:preferences],
-      )
-    end
-
-    article_clone_attachments(new_article.id)
-    article_clone_attachments_inline(new_article.id)
-  end
-
-  def article_body
-    body = NotificationFactory::Mailer.template(
-      templateInline: execution_data['body'],
-      objects:        notification_factory_template_objects,
-      quote:          true,
-      locale:         locale,
-      timezone:       timezone,
-    )
-
-    HtmlSanitizer.replace_inline_images(body, id)
+    article.history_change_source_attribute(performable, 'created')
+    article.save!
   end
 end
