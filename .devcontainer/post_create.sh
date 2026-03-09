e95762ccd4c448184c143c370dfe3fdfd7872cdd
Dusan Vuckovic <dv@zammad.com>
2026-03-09 14:59:43 +0000

    Maintenance: Devcontainer - Improve post create script.

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,8 +7,22 @@ sudo chown -R "${USER}" node_modules
 
 bin/setup --skip-server
 
+echo "== Precompile Ruby cache =="
+
 bundle exec bootsnap precompile --gemfile app/ lib/
+
+echo "== Configure Elasticsearch URL =="
 
 bundle exec rails r "Setting.set('es_url', 'http://elasticsearch:9200')"
 
+echo "== Run auto_wizard setup =="
+
 bundle exec rails zammad:setup:auto_wizard
+
+echo "== Rebuild search index =="
+
+bundle exec rails zammad:searchindex:rebuild
+
+echo "== Precompile frontend assets =="
+
+bundle exec rails assets:precompile
--- .devcontainer/with-mailserver/scripts/post_create.sh
+++ .devcontainer/with-mailserver/scripts/post_create.sh
@@ -7,9 +7,15 @@ sudo chown -R "${USER}" node_modules
 
 bin/setup --skip-server
 
+echo "== Precompile Ruby cache =="
+
 bundle exec bootsnap precompile --gemfile app/ lib/
 
+echo "== Configure Elasticsearch URL =="
+
 bundle exec rails r "Setting.set('es_url', 'http://elasticsearch:9200')"
+
+echo "== Run auto_wizard setup =="
 
 bundle exec rails zammad:setup:auto_wizard[.devcontainer/with-mailserver/auto_wizard_mailserver.json]
 
@@ -17,5 +23,14 @@ CERT_PATH=.devcontainer/with-mailserver/certs/mail.test.local.crt
 
 # Import self-signed certificate for mailserver.
 if [ -f "$CERT_PATH" ]; then
+  echo "== Import self-signed certificate for mailserver =="
   bundle exec rails r "SSLCertificate.create!(certificate: Rails.root.join('$CERT_PATH').read)"
 fi
+
+echo "== Rebuild search index =="
+
+bundle exec rails zammad:searchindex:rebuild
+
+echo "== Precompile frontend assets =="
+
+bundle exec rails assets:precompile
