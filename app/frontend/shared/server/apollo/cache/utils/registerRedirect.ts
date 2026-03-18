928e8a65d69b90f50f01fbd78efa214752698bad
Benjamin Scharf <bs@zammad.com>
2026-03-18 15:00:24 +0100

    Maintenance: Introduce dedicated utility for registering type-level merge policies

--- app/frontend/shared/server/apollo/cache/utils/registerRedirect.ts
+++ app/frontend/shared/server/apollo/cache/utils/registerRedirect.ts
@@ -5,7 +5,7 @@ import type { InMemoryCacheConfig } from '@apollo/client/cache/inmemory/types'
 // If a new array is returned and length differs, cache doesn't like it,
 // and we need to tell apollo that it needs to just replace current state altogether
 // this should be used only, if you are not using pagination.
-export default function registerIncomingMerge(
+export default function registerIncomingQueryMerge(
   config: InMemoryCacheConfig,
   fieldName: string,
   referenceTypename: string,
