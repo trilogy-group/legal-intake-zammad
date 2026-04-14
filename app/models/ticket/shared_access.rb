9da5ff36242ac8e5bca6046d95f4c6f3f2c5c863
Dhairya <dhairya.shah@trilogy.com>
2026-04-14 12:00:24 +0530

    Send email notification to customer at the moment a ticket is shared with them
    The shared customer now receives both an in-app notification (OnlineNotification)
    and an email with ticket details and a direct link when a ticket is shared.

--- app/models/ticket/shared_access.rb
+++ app/models/ticket/shared_access.rb
@@ -43,5 +43,40 @@ class Ticket::SharedAccess < ApplicationModel
       created_by_id: created_by_id,
       user_id:       user_id,
     )
+    send_share_email
+  end
+
+  def send_share_email
+    return if user.email.blank?
+
+    shared_by = User.find(created_by_id)
+
+    subject = NotificationFactory::Mailer.template(
+      templateInline: 'A ticket has been shared with you (#{ticket.title})',
+      objects:        { ticket: ticket, recipient: user, shared_by: shared_by },
+      quote:          false,
+    )
+
+    body = NotificationFactory::Mailer.template(
+      templateInline: '<div>Hi #{recipient.firstname},</div><br>' \
+                      '<div><b>#{shared_by.fullname}</b> has shared ticket ' \
+                      '<b>(#{config.ticket_hook}#{ticket.number})</b> with you.</div><br>' \
+                      '<div>You now have access to read and comment on this ticket.</div><br>' \
+                      '<div>To view the ticket, click on the following link:<br>' \
+                      '<a href="#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}">' \
+                      '#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}</a></div><br>' \
+                      '<div>Your #{config.product_name} Team</div>',
+      objects:        { ticket: ticket, recipient: user, shared_by: shared_by },
+      quote:          true,
+    )
+
+    NotificationFactory::Mailer.deliver(
+      recipient:    user,
+      subject:      subject,
+      body:         body,
+      content_type: 'text/html',
+      message_id:   "<ticket_shared.#{ticket.id}.#{user.id}.#{SecureRandom.uuid}@#{Setting.get('fqdn')}>",
+      references:   ticket.get_references,
+    )
   end
 end
